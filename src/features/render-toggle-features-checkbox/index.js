ff4a3563a5dbad5539086db41da242db4c7bc26d
Kai Hao <kevin830726@gmail.com>
2020-03-16 21:39:36 +0800

    Refactor render toggle

--- src/features/render-toggle-features-checkbox/index.js
+++ src/features/render-toggle-features-checkbox/index.js
@@ -1,6 +1,7 @@
 import './content.css';
 
 const CLASS_NAME = 'toggle-unbiased-github-checkbox';
+
 const STICKY_HEADER_SELECTOR = '.gh-header-sticky:not(.is-placeholder) .d-flex';
 const TOP_HEADER_ACTIONS_SELECTOR = '.gh-header-actions';
 const STICKY_REVIEW_ACTIONS_SELECTOR = '.js-reviews-container';
@@ -8,7 +9,7 @@ const NEW_BUTTON_SELECTOR = '[data-hotkey="c"]';
 
 const checkboxElements = {};
 
-function createCheckbox(name, onChange) {
+function createCheckbox(onChange) {
   const label = document.createElement('label');
   label.className = `${CLASS_NAME} btn btn-sm tooltipped tooltipped-w`;
   label.setAttribute('aria-label', 'Toggle Unbiased GitHub');
@@ -22,55 +23,66 @@ function createCheckbox(name, onChange) {
   label.appendChild(input);
   label.appendChild(img);
 
-  checkboxElements[name] = input;
-
   return label;
 }
+
+function insertAndReturnCheckbox(selector, placement, onChange) {
+  const element = document.querySelector(selector);
+
+  if (!element) {
+    return;
+  }
+
+  let checkbox = element.querySelector(`.${CLASS_NAME}`);
+
+  if (!checkbox) {
+    checkbox = createCheckbox(onChange);
+
+    placement(element, checkbox);
+  }
+
+  return checkbox;
+}
+
+const CHECKBOXES = [
+  {
+    selector: STICKY_HEADER_SELECTOR,
+    placement: (element, checkbox) => element.appendChild(checkbox),
+  },
+  {
+    selector: TOP_HEADER_ACTIONS_SELECTOR,
+    placement: (element, checkbox) => element.prepend(checkbox),
+  },
+  {
+    selector: STICKY_REVIEW_ACTIONS_SELECTOR,
+    placement: (element, checkbox) => element.prepend(checkbox),
+  },
+  {
+    selector: NEW_BUTTON_SELECTOR,
+    placement: (element, checkbox) => {
+      // Only append when there's no top header, i.e. in issues/PRs page
+      const topHeader = document.querySelector(TOP_HEADER_ACTIONS_SELECTOR);
+      if (topHeader && topHeader.querySelector(`.${CLASS_NAME}`)) {
+        return;
+      }
+
+      checkbox.classList.remove('btn-sm');
+
+      element.insertAdjacentElement('beforebegin', checkbox);
+    },
+  },
+];
 
 export default function renderToggleFeaturesCheckbox(isEnabled, setIsEnabled) {
   function handleChangeCheckbox(e) {
     setIsEnabled(e.target.checked);
   }
 
-  const stickyHeader = document.querySelector(STICKY_HEADER_SELECTOR);
-  if (stickyHeader && !checkboxElements.stickyHeader) {
-    const stickyCheckbox = createCheckbox('stickyHeader', handleChangeCheckbox);
-
-    stickyHeader.appendChild(stickyCheckbox);
-  }
-
-  const topActionsHeader = document.querySelector(TOP_HEADER_ACTIONS_SELECTOR);
-  if (topActionsHeader && !checkboxElements.topActionsHeader) {
-    const topCheckbox = createCheckbox(
-      'topActionsHeader',
-      handleChangeCheckbox
-    );
-
-    topActionsHeader.prepend(topCheckbox);
-  }
-
-  const stickyReviewActions = document.querySelector(
-    STICKY_REVIEW_ACTIONS_SELECTOR
-  );
-  if (stickyReviewActions && !checkboxElements.stickyReviewActions) {
-    const stickyReviewCheckbox = createCheckbox(
-      'stickyReviewActions',
-      handleChangeCheckbox
-    );
-
-    stickyReviewActions.prepend(stickyReviewCheckbox);
-  }
-
-  const newButton = document.querySelector(NEW_BUTTON_SELECTOR);
-  // Only append when there's no top header, i.e. in issues/PRs page
-  if (!topActionsHeader && newButton && !checkboxElements.newButton) {
-    const newButtonCheckbox = createCheckbox('newButton', handleChangeCheckbox);
-    newButtonCheckbox.classList.remove('btn-sm');
-
-    newButton.insertAdjacentElement('beforebegin', newButtonCheckbox);
-  }
-
-  Object.values(checkboxElements).forEach(checkbox => {
-    checkbox.checked = isEnabled;
-  });
+  CHECKBOXES.map(({ selector, placement }) =>
+    insertAndReturnCheckbox(selector, placement, handleChangeCheckbox)
+  )
+    .filter(Boolean)
+    .forEach(checkbox => {
+      checkbox.querySelector('input').checked = isEnabled;
+    });
 }
