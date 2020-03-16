bd76a23019f6bdd5092c4fbff586c9e264f5414f
Kai Hao <kevin830726@gmail.com>
2020-03-16 20:49:05 +0800

    Fix toggle button in review page

--- src/features/render-toggle-features-checkbox/index.js
+++ src/features/render-toggle-features-checkbox/index.js
@@ -2,7 +2,8 @@ import './content.css';
 
 const CLASS_NAME = 'toggle-unbiased-github-checkbox';
 const STICKY_HEADER_SELECTOR = '.gh-header-sticky:not(.is-placeholder) .d-flex';
-const TOP_HEADER_SELECTOR = '.gh-header-actions';
+const TOP_HEADER_ACTIONS_SELECTOR = '.gh-header-actions';
+const STICKY_REVIEW_ACTIONS_SELECTOR = '.js-reviews-container';
 const NEW_BUTTON_SELECTOR = '[data-hotkey="c"]';
 
 const checkboxElements = {};
@@ -38,16 +39,31 @@ export default function renderToggleFeaturesCheckbox(isEnabled, setIsEnabled) {
     stickyHeader.appendChild(stickyCheckbox);
   }
 
-  const topHeader = document.querySelector(TOP_HEADER_SELECTOR);
-  if (topHeader && !checkboxElements.topHeader) {
-    const topCheckbox = createCheckbox('topHeader', handleChangeCheckbox);
+  const topActionsHeader = document.querySelector(TOP_HEADER_ACTIONS_SELECTOR);
+  if (topActionsHeader && !checkboxElements.topActionsHeader) {
+    const topCheckbox = createCheckbox(
+      'topActionsHeader',
+      handleChangeCheckbox
+    );
 
-    topHeader.prepend(topCheckbox);
+    topActionsHeader.prepend(topCheckbox);
+  }
+
+  const stickyReviewActions = document.querySelector(
+    STICKY_REVIEW_ACTIONS_SELECTOR
+  );
+  if (stickyReviewActions && !checkboxElements.stickyReviewActions) {
+    const stickyReviewCheckbox = createCheckbox(
+      'stickyReviewActions',
+      handleChangeCheckbox
+    );
+
+    stickyReviewActions.prepend(stickyReviewCheckbox);
   }
 
   const newButton = document.querySelector(NEW_BUTTON_SELECTOR);
   // Only append when there's no top header, i.e. in issues/PRs page
-  if (!topHeader && newButton && !checkboxElements.newButton) {
+  if (!topActionsHeader && newButton && !checkboxElements.newButton) {
     const newButtonCheckbox = createCheckbox('newButton', handleChangeCheckbox);
     newButtonCheckbox.classList.remove('btn-sm');
 
